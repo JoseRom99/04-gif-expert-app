a2352ca428a0dc030682a97346484d48bdcaad03
JoseRom99 <joseromeromohedano99@gmail.com>
2022-10-18 19:26:56 +0200

    04-react-app

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {GifExpertApp} from './GifExpertApp'
+import './styles.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <GifExpertApp />
+  </React.StrictMode>
+)
+//8yacke3djxk9BX3HCilwtXw6jpTdSBsh
